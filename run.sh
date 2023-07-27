75c7035c825c97702e978a8227f7c035c41c58d6
Amit Pareek <amit@cakewalk.in>
2023-07-27 20:58:27 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-pg_dump ${DUMP_FLAGS} "${SRC_POSTGRES}" | psql "${DST_POSTGRES}"
+pg_dump ${DUMP_FLAGS} "${SRC_POSTGRES}" --file=dumpfile.bak -Fc -Z 6 -v
+pg_restore -d "${DST_POSTGRES}" -Fc --single-transaction -c --if-exists dumpfile.bak -v
